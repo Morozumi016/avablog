08157788414bd65959382c50eabb9bb3bb7b0021
Morozumi <shingo.morozumi@avasys.jp>
2016-06-22 04:37:31 +0000

    migrate commit

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160622025224) do
+ActiveRecord::Schema.define(version: 20160622042730) do
+
+  create_table "circles", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
