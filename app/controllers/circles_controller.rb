c8bc9decd39297399c0f7beab7c7d92b876d6ef6
Morozumi <shingo.morozumi@avasys.jp>
2016-06-23 02:20:43 +0000

    circleimagerec

--- app/controllers/circles_controller.rb
+++ app/controllers/circles_controller.rb
@@ -73,6 +73,6 @@ class CirclesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def circle_params
-      params.require(:circle).permit(:name)
+      params.require(:circle).permit(:name,:picture)
     end
 end
